dd993cca422fe791ac488e6dbbecda38592a1606
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-15 22:28:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,78 @@
+import re, os, time
+
+id_pattern = re.compile(r'^.\d+$') 
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+   
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
+    DB_URL  = os.environ.get("DB_URL","")
+ 
+    # other configs
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+
+    # wes response configuration     
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+
+
+class Txt(object):
+    # part of text configuration
+    START_TXT = """<b>Hᴀɪ {} 👋,
+Tʜɪs Is Aɴ Aᴅᴠᴀɴᴄᴇᴅ Aɴᴅ Yᴇᴛ Pᴏᴡᴇʀꜰᴜʟ Rᴇɴᴀᴍᴇ Bᴏᴛ
+Usɪɴɢ Tʜɪs Bᴏᴛ Yᴏᴜ Cᴀɴ Rᴇɴᴀᴍᴇ & Cʜᴀɴɢᴇ Tʜᴜᴍʙɴᴀɪʟ Oꜰ Yᴏᴜʀ Fɪʟᴇ
+Yᴏᴜ Cᴀɴ Aʟsᴏ Cᴏɴᴠᴇʀᴛ Vɪᴅᴇᴏ Tᴏ Fɪʟᴇ & Fɪʟᴇ Tᴏ Vɪᴅᴇᴏ
+Tʜɪs Bᴏᴛ Aʟꜱᴏ Sᴜᴘᴘᴏʀᴛs Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Aɴᴅ Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ</b>"""
+
+    ABOUT_TXT = """<b>╭───────────⍟
+├🤖 ᴍy ɴᴀᴍᴇ : {}
+├📕 Lɪʙʀᴀʀy : <a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>
+├✏️ Lᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pyᴛʜᴏɴ 3</a>
+├💾 Dᴀᴛᴀ Bᴀꜱᴇ: <a href=https://cloud.mongodb.com>Mᴏɴɢᴏ DB</a>
+├📊 Bᴜɪʟᴅ Vᴇʀꜱɪᴏɴ: <a href=https://github.com/>Pyʀᴏ Rᴇɴᴀᴍᴇʀ V3.0.0</a></b>     
+╰───────────────⍟ """
+
+    HELP_TXT = """
+🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
+  
+<b>•></b> /start Tʜᴇ Bᴏᴛ Aɴᴅ Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
+📑 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
+<b>•></b> /set_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Sᴇᴛ ᴀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /see_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /del_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+Exᴀᴍᴩʟᴇ:- /set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
+💾 Sɪᴢᴇ: {filesize}
+⏰ Dᴜʀᴀᴛɪᴏɴ: {duration}
+✏️ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
+<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nɴᴀᴍᴇ \nAɴᴅ Aᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ]"""
+
+
+    BOT_TXT = """<b><u>𝕄𝕆ℝ𝔼 𝔹𝕆𝕋𝕊 & ℂℍ𝔸ℕℕ𝔼𝕃'𝕊</b></u>
+    
+» 𝗠𝗢𝗥𝗘 𝗕𝗢𝗧𝗦 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 : <a href=https://t.me/+9Z1w2KOebaliYzdl>𝗶𝗣𝗔𝗣𝗞𝗢𝗥𝗡 𝗕𝗢𝗧𝗦</a>
+» 𝗦𝗛𝗢𝗣𝗣𝗜𝗡𝗚 𝗗𝗘𝗔𝗟𝗦 : <a href=https://t.me/+VnG269AYxSM3OGFl>𝗧𝗥𝗨𝗘 𝗗𝗘𝗔𝗟𝗦 𝗠𝗔𝗦𝗧𝗘𝗥</a>
+
+• 🤖 <a href=https://t.me/iPopkonBot>𝗕𝗢𝗧 𝟭</a>
+• 🤖 <a href=https://t.me/iPepkornBot>𝗕𝗢𝗧 𝟮</a>
+• 🤖 <a href=https://t.me/EvaMaria2Bot>𝗕𝗢𝗧 𝟯</a>
+
+• 🤖 <a href=https://t.me/+JhyQko8z2ilkZjJl>𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝟭</a>
+• 🤖 <a href=https://t.me/+luqXeffLeIg2ZGZl>𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝟮</a>
+"""
+
+    PROGRESS_BAR = """<b>\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ Dᴏɴᴇ : {0}%
+┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
+┣⪼ ⏰️ Eᴛᴀ: {4}
+╰━━━━━━━━━━━━━━━➣ </b>"""
